c7791e2126b9aacf06de750ae2958da79f18563b
s8w1e2ep <ha9mv8c@gmail.com>
2017-12-09 21:58:28 +0800

    Create a model class of statistic

---
+++ web/model/statistic.lib.php
@@ -0,0 +1,80 @@
+<?php
+// initialize
+require_once HOME_DIR . 'configs/config.php';
+/**
+ * 統計類別
+ */
+class Statistic
+{
+    public $db = null;
+    public $smarty = null;
+    public $msg = '';
+    public $error = '';
+    public $admin = null;
+
+    /**
+     * Statistic constructor
+     * @DateTime 2016-12-09
+     */
+    public function __construct() {
+        session_start();
+        // instantiate the pdo object
+        $this->db = dbSetup::getDbConn();
+        // instantiate the template object
+        $this->smarty = new SmartyConfig();
+    }
+
+    /**
+     * 顯示文章點閱率
+     * @DateTime 2016-12-09
+     */
+    public function viewArticleClick() {
+        if ($_SESSION['isLogin'] == true) {
+            $this->smarty->assign('error', $this->error);
+            $this->smarty->display('statistic/articleClick.html');
+        } else {
+            $this->error = '請先登入!';
+            $this->viewLogin();
+        }
+    }
+
+    /**
+     * 顯示商品點閱率
+     * @DateTime 2016-12-09
+     */
+    public function viewGoodsClick() {
+        if ($_SESSION['isLogin'] == true) {
+            $this->smarty->assign('error', $this->error);
+            $this->smarty->display('statistic/goodsClick.html');
+        } else {
+            $this->error = '請先登入!';
+            $this->viewLogin();
+        }
+    }
+
+    /**
+     * 顯示消費紀錄列表
+     * @DateTime 2016-12-09
+     */
+    public function viewOrderHistory() {
+        if ($_SESSION['isLogin'] == true) {
+            $this->smarty->assign('error', $this->error);
+            $this->smarty->display('statistic/orderHistory.html');
+        } else {
+            $this->error = '請先登入!';
+            $this->viewLogin();
+        }
+    }
+
+    /**
+     * 顯示登入畫面
+     * @DateTime 2017-12-09
+     */
+    public function viewLogin()
+    {
+        $_SESSION['isLogin'] = false;
+        $this->smarty->assign('error', $this->error);
+        $this->smarty->assign('homePath', APP_ROOT_DIR);
+        $this->smarty->display('login.html');
+    }
+}
